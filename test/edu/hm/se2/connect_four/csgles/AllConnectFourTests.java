8c761ffb93b68f5569652b86d1ac07e666567e95
Chris <christoph.schwarz.2002@gmail.com>
2021-04-27 10:33:51 +0200

    Updated AllConnectFourTest.java

--- test/edu/hm/se2/connect_four/csgles/AllConnectFourTests.java
+++ test/edu/hm/se2/connect_four/csgles/AllConnectFourTests.java
@@ -1,5 +1,6 @@
 package edu.hm.se2.connect_four.csgles;
 
+import edu.hm.se2.connect_four.csgles.datastore.ConnectFourFieldTest;
 import edu.hm.se2.connect_four.csgles.datastore.mutable.FullConnectFourBoardTest;
 import edu.hm.se2.connect_four.csgles.datastore.mutable.FullConnectFourGameTest;
 import edu.hm.se2.connect_four.csgles.datastore.mutable.FullConnectFourPlayerTest;
@@ -7,6 +8,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({FullConnectFourBoardTest.class, FullConnectFourGameTest.class, FullConnectFourPlayerTest.class})
+@Suite.SuiteClasses({FullConnectFourBoardTest.class, FullConnectFourGameTest.class, FullConnectFourPlayerTest.class, ConnectFourFieldTest.class})
 public class AllConnectFourTests {
 }
