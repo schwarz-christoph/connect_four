7c172dc89b06aabeba7678e6d4a63559451bba23
Equelz <equelz@web.de>
2021-05-11 10:53:29 +0200

    Revert "Delete Logic"
    This reverts commit b964abe3068bea8e27c11786214ab76b1988f9e3.

---
+++ src/edu/hm/scholz/enno/connect_four/logic/Move.java
@@ -0,0 +1,13 @@
+package edu.hm.se2.connect_four.csgles.logic;
+
+/**
+ * All the moves that can be made.
+ * @author Christoph Schwarz (schwarz4@hm.edu)
+ * @author Georg Lang (glang@hm.edu)
+ * @author Enno Scholz (enno.scholz@hm.edu)
+ * @version 04-01-2021
+ */
+
+public enum Move {
+    UP, DOWN, LEFT, RIGHT, CONFIRM;
+}
