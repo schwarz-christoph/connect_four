055026b7f685cd783c80dfb92d5a37ae216d87d9
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2021-05-21 12:57:44 +0200

    Implemented View

---
+++ src/edu/hm/scholz/enno/connect_four/view/TextDump.java
@@ -0,0 +1,29 @@
+package edu.hm.scholz.enno.connect_four.view;
+
+import edu.hm.scholz.enno.connect_four.datastore.Board;
+import edu.hm.scholz.enno.connect_four.datastore.Game;
+import edu.hm.scholz.enno.connect_four.datastore.Observer;
+import edu.hm.scholz.enno.connect_four.datastore.Player;
+
+public class TextDump implements Observer {
+
+    @Override
+    public void updateCursor(Board board) {
+
+    }
+
+    @Override
+    public void updateMatrix(Board board, Player player) {
+
+    }
+
+    @Override
+    public void updateWinner(Game game) {
+
+    }
+
+    @Override
+    public void updatePlayerSelect(Game game) {
+
+    }
+}
