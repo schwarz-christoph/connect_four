2a8e3f4df48cd42c50151b6bc616b546ab724592
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2021-05-21 14:09:41 +0200

    Edited corsep. to Ennos Comments

---
+++ src/edu/hm/scholz/enno/connect_four/logic/LogicFactory.java
@@ -0,0 +1,24 @@
+package edu.hm.scholz.enno.connect_four.logic;
+
+import edu.hm.scholz.enno.connect_four.datastore.PlayerID;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.FullBoard;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.FullGame;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.FullPlayer;
+
+public interface Factory {
+    /**
+     * Make a GameManager
+     *
+     * @param game the Game witch needs the rules
+     * @return the logic of the game with two independent players
+     */
+    static GameManager makeGameManager(FullBoard board, FullGame game) {
+
+        FullPlayer player1 = edu.hm.scholz.enno.connect_four.datastore.mutable.Factory.makePlayer(PlayerID.PLAYER_1);
+
+        FullPlayer player2 = edu.hm.scholz.enno.connect_four.datastore.mutable.Factory.makePlayer(PlayerID.PLAYER_2);
+
+        return new ConnectFourManager(board, game, player1, player2);
+
+    }
+}
