830e2a937c1b5f4a367fdcd172615cce33f264e5
georgLang9 <langggeorg61@gmail.com>
2021-05-29 15:50:30 +0200

    Implemented Tests for execute Move

--- test/edu/hm/scholz/enno/connect_four/logic/ExecuteMoveTest.java
+++ test/edu/hm/scholz/enno/connect_four/logic/ExecuteMoveTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.rules.Timeout;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,6 +22,28 @@ public class ExecuteMoveTest {
 
     @Test
     public void executeMoveTest(){
+        int xBefore = 1;
+
+        //arrange
+        FullGame game = Factory.makeGame(PlayerID.PLAYER_1);
+        game.setIsStarted(true);
+        FullBoard board = Factory.makeBoard();
+        GameManager manager = LogicFactory.makeGameManager(board, game);
+
+        board.setHighlight(List.of(Factory.makeField(xBefore, 1, PlayerID.NONE), Factory.makeField(xBefore, 2, PlayerID.NONE), Factory.makeField(xBefore, 3, PlayerID.NONE), Factory.makeField(xBefore, 4, PlayerID.NONE), Factory.makeField(xBefore, 5, PlayerID.NONE), Factory.makeField(xBefore, 6, PlayerID.NONE), Factory.makeField(xBefore, 7, PlayerID.NONE)));
+        ArrayList<Field> want = new ArrayList<>();
+        Collections.addAll(want, Factory.makeField(xBefore, 0, PlayerID.PLAYER_1));
+        List<Field> have = board.getFields();
+
+        //act
+        manager.executeMove(Move.CONFIRM, game.getActivePlayer());
+
+        //assert
+        assertEquals(want, have);
+    }
+
+    @Test
+    public void executeMoveGameNotStartedTest(){
         int xBefore = 1;
 
         //arrange
@@ -37,7 +60,33 @@ public class ExecuteMoveTest {
         manager.executeMove(Move.CONFIRM, game.getActivePlayer());
 
         //assert
-        assertEquals(want, have);
+        assertFalse(manager.executeMove(Move.CONFIRM, game.getActivePlayer()));
+    }
+
+    @Test
+    public void executeMoveNotAllowedMoveTest(){
+        //arrange
+        FullGame game = Factory.makeGame(PlayerID.PLAYER_1);
+        game.setIsStarted(true);
+        FullBoard board = Factory.makeBoard();
+        GameManager manager = LogicFactory.makeGameManager(board, game);
+        board.setHighlight(new ArrayList<>(Arrays.asList(Factory.makeField(3, 1, PlayerID.PLAYER_1))));
+
+        //act, assert
+        assertFalse(manager.executeMove(Move.DOWN, PlayerID.PLAYER_1));
+    }
+
+    @Test
+    public void executeMoveAllowedMoveTest(){
+        //arrange
+        FullGame game = Factory.makeGame(PlayerID.PLAYER_1);
+        game.setIsStarted(true);
+        FullBoard board = Factory.makeBoard();
+        GameManager manager = LogicFactory.makeGameManager(board, game);
+        board.setHighlight(new ArrayList<>(Arrays.asList(Factory.makeField(3, 1, PlayerID.PLAYER_1))));
+
+        //act, assert
+        assertFalse(manager.executeMove(Move.CONFIRM, PlayerID.PLAYER_1));
     }
 
 }
