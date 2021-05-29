25e6826ab5f49eda046137dddb0abcc221a6c89b
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2021-05-29 17:36:38 +0200

    Added Logic Test to Testsuite

--- test/edu/hm/scholz/enno/connect_four/AllConnectFourTests.java
+++ test/edu/hm/scholz/enno/connect_four/AllConnectFourTests.java
@@ -4,10 +4,12 @@ import edu.hm.scholz.enno.connect_four.datastore.ConnectFourFieldTest;
 import edu.hm.scholz.enno.connect_four.datastore.mutable.FullConnectFourBoardTest;
 import edu.hm.scholz.enno.connect_four.datastore.mutable.FullConnectFourGameTest;
 import edu.hm.scholz.enno.connect_four.datastore.mutable.FullConnectFourPlayerTest;
+import edu.hm.scholz.enno.connect_four.logic.ConnectFourManager;
+import edu.hm.scholz.enno.connect_four.logic.ConnectFourManagerTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({FullConnectFourBoardTest.class, FullConnectFourGameTest.class, FullConnectFourPlayerTest.class, ConnectFourFieldTest.class})
+@Suite.SuiteClasses({FullConnectFourBoardTest.class, FullConnectFourGameTest.class, FullConnectFourPlayerTest.class, ConnectFourFieldTest.class, ConnectFourManagerTest.class})
 public class AllConnectFourTests {
 }
