0f464b71352fac907c86cde0668debe6cdb91408
Equelz <equelz@web.de>
2021-05-30 15:12:27 +0200

    Fixed highlight not working in Menu

--- src/edu/hm/scholz/enno/connect_four/view/TextDump.java
+++ src/edu/hm/scholz/enno/connect_four/view/TextDump.java
@@ -2,7 +2,6 @@ package edu.hm.scholz.enno.connect_four.view;
 
 import edu.hm.scholz.enno.connect_four.datastore.*;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -20,7 +19,7 @@ public class TextDump implements Observer {
                 gameText = getEndScreenString(game);
             } else {
                 // regular game
-                gameText = getGameString(board, player1, player2);
+                gameText = getGameString(board, game, player1, player2);
             }
         } else {
             // player select screen
@@ -47,63 +46,88 @@ public class TextDump implements Observer {
         return builder.toString();
     }
 
-    private String getGameString(Board board, Player player1, Player player2) {
+    private String getGameString(Board board, Game game, Player player1, Player player2) {
         final StringBuilder builder = new StringBuilder();
         builder.append(indexLine);
         builder.append(frameLine);
-        builder.append(getMenuString(player1, player2));
+        builder.append(getMenuString(board, game, player1, player2));
 
-        IntStream.range(1, 8).forEach(index -> builder.append(index).append(getGameLineString(board, index)));
+        IntStream.range(1, 8).forEach(index -> builder.append(index).append(getGameLineString(board, game, index)));
 
         builder.append(frameLine);
         return builder.toString();
     }
 
-    private String getGameLineString(Board board, int lineIndex) {
+    private String getGameLineString(Board board, Game game, int lineIndex) {
         final StringBuilder builder = new StringBuilder();
         builder.append('|');
-        IntStream.range(0, 8).forEach(index -> builder.append(getCharForField(board, index, lineIndex)).append(' '));
+        IntStream.range(0, 8).forEach(index -> builder.append(getCharForField(board, game, index, lineIndex)).append(' '));
         builder.append('|');
         builder.append('\n');
         return builder.toString();
     }
 
-    private char getCharForField(Board board, int xCoordinate, int yCoordinate) {
+    private char getCharForField(Board board, Game game, int xCoordinate, int yCoordinate) {
         final char fieldChar;
+
         final Optional<Field> selectedFieldOptional = board.getFields().stream()
                 .filter(field -> field.xCoordinate() == xCoordinate)
                 .filter(field -> field.yCoordinate() == yCoordinate)
                 .findFirst();
+        final PlayerID occupyingPlayer = selectedFieldOptional.isPresent() ?
+                selectedFieldOptional.get().owner() :
+                PlayerID.NONE;
 
-        final PlayerID occupyingPlayer;
-        final boolean isFieldHighlighted;
-        if(selectedFieldOptional.isPresent()) {
-            final Field selectedField = selectedFieldOptional.get();
-            occupyingPlayer = selectedField.owner();
-            isFieldHighlighted = board.getHighlight().stream()
-                    .filter(field -> field.xCoordinate() == selectedField.xCoordinate())
-                    .anyMatch(field -> field.xCoordinate() == selectedField.xCoordinate());
-        } else {
-            occupyingPlayer = PlayerID.NONE;
-            isFieldHighlighted = false;
-        }
+        final boolean isFieldHighlighted = board.getHighlight().stream()
+                .filter(field -> field.xCoordinate() == xCoordinate)
+                .anyMatch(field -> field.yCoordinate() == yCoordinate);
+        final char activePlayerHighlightChar = game.getActivePlayer() == PlayerID.PLAYER_1 ? 'g' : 'b';
 
         switch (occupyingPlayer) {
-            case PLAYER_1 -> fieldChar = isFieldHighlighted ? 'g' : 'G';
-            case PLAYER_2 -> fieldChar = isFieldHighlighted ? 'b' : 'B';
-            default -> fieldChar = ' ';
+            case PLAYER_1 -> fieldChar = 'G';
+            case PLAYER_2 -> fieldChar = 'B';
+            default -> fieldChar = isFieldHighlighted ? activePlayerHighlightChar : ' ';
         }
         return fieldChar;
     }
 
-    private String getMenuString(Player player1, Player player2) {
-        return "0|" +
-                (player1.isBombJokerUsed() ? "O-" : "O ") +
-                (player1.isDeleteJokerUsed() ? "C-" : "C ") +
-                "  R Y   " +
-                (player2.isDeleteJokerUsed() ? "C-" : "C ") +
-                (player2.isBombJokerUsed() ? "O-" : "O |") +
-                '\n';
+    private String getMenuString(Board board, Game game, Player player1, Player player2) {
+        final char activePlayerHighlightChar = game.getActivePlayer() == PlayerID.PLAYER_1 ? 'g' : 'b';
+        final Field highlight = board.getHighlight().get(0);
+        final boolean isMenuHighlight = highlight.yCoordinate() == 0;
+
+        final String index = "0";
+        final char frame = '|';
+        final String player1BombJoker =
+                getJokerString(true, player1.isBombJokerUsed(), isMenuHighlight && highlight.xCoordinate() == 0);
+        final String player1DeleteJoker =
+                getJokerString(false, player1.isDeleteJokerUsed(), isMenuHighlight && highlight.xCoordinate() == 1);
+        final String emptySpaceLeft = (isMenuHighlight && highlight.xCoordinate() == 2) ? activePlayerHighlightChar + " " : "  ";
+        final String resetButton = (isMenuHighlight && highlight.xCoordinate() == 3) ? "y " : "Y ";
+        final String exitButton = (isMenuHighlight && highlight.xCoordinate() == 4) ? "r " : "R ";
+        final String emptySpaceRight = (isMenuHighlight && highlight.xCoordinate() == 5) ? activePlayerHighlightChar + " " : "  ";
+        final String player2DeleteJoker =
+                getJokerString(false, player2.isDeleteJokerUsed(), isMenuHighlight && highlight.xCoordinate() == 6);
+        final String player2BombJoker =
+                getJokerString(true, player2.isBombJokerUsed(), isMenuHighlight && highlight.xCoordinate() == 7);
+
+        return index + frame +
+                player1BombJoker +
+                player1DeleteJoker +
+                emptySpaceLeft +
+                resetButton +
+                exitButton +
+                emptySpaceRight +
+                player2DeleteJoker +
+                player2BombJoker +
+                frame + '\n';
+    }
+
+    private String getJokerString(boolean isBomb, boolean isUsed, boolean isHighlighted) {
+        String color = isBomb ? "O" : "C";
+        color = isHighlighted ? color.toLowerCase() : color;
+        color += isUsed ? '-' : ' ';
+        return color;
     }
 
     private String getPlayerSelectString(Game game) {
