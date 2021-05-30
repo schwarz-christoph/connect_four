2927281c141f82478aa887e58743818397dfb932
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2021-05-30 14:42:49 +0200

    Added DeleteJokerHighlightTest to TetstSuite (Logic)

--- test/edu/hm/scholz/enno/connect_four/logic/ConnectFourManagerTest.java
+++ test/edu/hm/scholz/enno/connect_four/logic/ConnectFourManagerTest.java
@@ -3,11 +3,16 @@ package edu.hm.scholz.enno.connect_four.logic;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import java.awt.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ExecuteMoveTest.class, GetMovesMenuTest.class, HighlightTest.class, GetMovesRegularTest.class,
-        ExecuteMoveWinningConditionsTest.class, GetMovesPlayerSelectAndEndScreenTest.class, MenuTest.class,
-        ConnectFourManagerConstructorTest.class})
+@Suite.SuiteClasses({ExecuteMoveTest.class,
+        GetMovesMenuTest.class,
+        HighlightTest.class,
+        GetMovesRegularTest.class,
+        ExecuteMoveWinningConditionsTest.class,
+        GetMovesPlayerSelectAndEndScreenTest.class,
+        MenuTest.class,
+        ConnectFourManagerConstructorTest.class,
+        DeleteJokerHighlightTest.class})
 public class ConnectFourManagerTest {
 }
