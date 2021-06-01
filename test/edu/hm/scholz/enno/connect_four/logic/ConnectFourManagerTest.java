857c3fd1b3e1899120cd1bb2bda9c4fec32ec5cb
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2021-06-01 11:42:42 +0200

    Added MoveTest class

--- test/edu/hm/scholz/enno/connect_four/logic/ConnectFourManagerTest.java
+++ test/edu/hm/scholz/enno/connect_four/logic/ConnectFourManagerTest.java
@@ -14,6 +14,7 @@ import org.junit.runners.Suite;
         MenuTest.class,
         bombJokerTest.class,
         ConnectFourManagerConstructorTest.class,
-        DeleteJokerHighlightTest.class})
+        DeleteJokerHighlightTest.class,
+        MoveTest.class})
 public class ConnectFourManagerTest {
 }
