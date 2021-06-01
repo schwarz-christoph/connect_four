5c4602c0ebea23688fdf5c21bc214ed0a988fded
Equelz <equelz@web.de>
2021-06-01 10:35:45 +0200

    Added method to get Field List to TestUtility

--- test/edu/hm/scholz/enno/connect_four/TestUtility.java
+++ test/edu/hm/scholz/enno/connect_four/TestUtility.java
@@ -6,13 +6,21 @@ import edu.hm.scholz.enno.connect_four.datastore.PlayerID;
 import edu.hm.scholz.enno.connect_four.datastore.mutable.Factory;
 import edu.hm.scholz.enno.connect_four.datastore.mutable.FullBoard;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class TestUtility {
-    public static void createBoardState(FullBoard board, String boardState) {
+    public static List<Field> getOccupiedFieldList(String boardState) {
+        final List<Field> allOccupiedFields = new ArrayList<>();
         IntStream.range(0, boardState.length())
                 .filter(index -> boardState.charAt(index) != '.')
-                .forEach(index -> board.placeStone(generateField(index, boardState.charAt(index))));
+                .forEach(index -> allOccupiedFields.add(generateField(index, boardState.charAt(index))));
+        return allOccupiedFields;
+    }
+
+    public static void createBoardState(FullBoard board, String boardState) {
+        getOccupiedFieldList(boardState).forEach(board::placeStone);
     }
 
     private static Field generateField(int index, char owningPlayer) {
