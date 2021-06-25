49ae03c45a13c0cd32b806e37a4c604c00ec42b1
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2021-06-25 11:35:21 +0200

    Implemented ValidatingView and BotTheorieTest

---
+++ src/edu/hm/scholz/enno/connect_four/view/ValidatingView.java
@@ -0,0 +1,125 @@
+package edu.hm.scholz.enno.connect_four.view;
+
+import edu.hm.scholz.enno.connect_four.datastore.*;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.Factory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ValidatingView implements View{
+
+    private boolean isValidGame = false;
+
+    public ValidatingView(Game game) {
+        if (game == null)
+            throw new IllegalArgumentException("Arguments cannot be null.");
+
+        game.register(this);
+    }
+
+    @Override
+    public void update(Board board, Game game, Player player1, Player player2) {
+        if(game.getActivePlayer() == PlayerID.NONE){
+            validateGame(board, game);
+        }
+    }
+
+    @Override
+    public void shut() {
+        View.super.shut();
+    }
+
+    public boolean isValidGame() {
+        return isValidGame;
+    }
+
+    private void validateGame(Board board, Game game){
+        final PlayerID wantPlayerID;
+        final boolean winnerPlayer1 = validateWinner(board, PlayerID.PLAYER_1);
+        final boolean winnerPlayer2 = validateWinner(board, PlayerID.PLAYER_2);
+        final List<Field> fields = board.getFields();
+        final int fieldSize = 8;
+        final boolean boardFull = fields.size() == fieldSize * (fieldSize - 1);
+        if (winnerPlayer1 ^ winnerPlayer2) // ^ = exclusive or
+            wantPlayerID = winnerPlayer1 ? PlayerID.PLAYER_1 : PlayerID.PLAYER_2;
+        else
+            wantPlayerID = PlayerID.NONE;
+
+        if(game.getWinner() == wantPlayerID){
+            isValidGame = true;
+        }
+
+        if(boardFull){
+            isValidGame = game.getActivePlayer() == PlayerID.NONE;
+        }
+    }
+
+    private boolean validateWinner(Board board, PlayerID wantPlayerID){
+        final List<Field> fields = board.getFields();
+
+        final List<Field> playerFields = fields.stream()
+                .filter(field -> field.owner() == wantPlayerID)
+                .collect(Collectors.toList());
+
+        return containsWinningSequence(playerFields);
+    }
+
+    private static boolean containsWinningSequence(List<Field> fields) {
+        final boolean result;
+
+        if (winningSequenceHorizontal(fields))
+            result = true;
+        else if (winningSequenceVertical(fields))
+            result = true;
+        else if (winningSequenceDiagonalDownward(fields))
+            result = true;
+        else result = winningSequenceDiagonalUpward(fields);
+
+        return result;
+    }
+
+
+    private static boolean winningSequenceHorizontal(List<Field> fields) {
+        final int maxAdderValue = 3;
+        final int fieldSize = 8;
+
+        return fields.stream()
+                .filter(field -> field.xCoordinate() < fieldSize - maxAdderValue)
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate() + 1, field.yCoordinate(), field.owner())))
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate() + 2, field.yCoordinate(), field.owner())))
+                .anyMatch(field -> fields.contains(Factory.makeField(field.xCoordinate() + maxAdderValue, field.yCoordinate(), field.owner())));
+    }
+
+    private static boolean winningSequenceVertical(List<Field> fields) {
+        final int maxAdderValue = 3;
+        final int fieldSize = 8;
+
+        return fields.stream()
+                .filter(field -> field.yCoordinate() < fieldSize - maxAdderValue)
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate(), field.yCoordinate() + 1, field.owner())))
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate(), field.yCoordinate() + 2, field.owner())))
+                .anyMatch(field -> fields.contains(Factory.makeField(field.xCoordinate(), field.yCoordinate() + maxAdderValue, field.owner())));
+    }
+
+    private static boolean winningSequenceDiagonalDownward(List<Field> fields) {
+        final int minAdderValue = 2;
+        final int maxAdderValue = 3;
+
+        return fields.stream()
+                .filter(field -> field.xCoordinate() > minAdderValue)
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate() - 1, field.yCoordinate() - 1, field.owner())))
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate() - 2, field.yCoordinate() - 2, field.owner())))
+                .anyMatch(field -> fields.contains(Factory.makeField(field.xCoordinate() - maxAdderValue, field.yCoordinate() - maxAdderValue, field.owner())));
+    }
+
+    private static boolean winningSequenceDiagonalUpward(List<Field> fields) {
+        final int maxAdderValue = 3;
+        final int fieldSize = 8;
+
+        return fields.stream()
+                .filter(field -> field.xCoordinate() < fieldSize - maxAdderValue)
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate() + 1, field.yCoordinate() - 1, field.owner())))
+                .filter(field -> fields.contains(Factory.makeField(field.xCoordinate() + 2, field.yCoordinate() - 2, field.owner())))
+                .anyMatch(field -> fields.contains(Factory.makeField(field.xCoordinate() + maxAdderValue, field.yCoordinate() - maxAdderValue, field.owner())));
+    }
+}
---
+++ test/edu/hm/scholz/enno/connect_four/control/BotTheorieTest.java
@@ -0,0 +1,78 @@
+package edu.hm.scholz.enno.connect_four.control;
+
+import edu.hm.scholz.enno.connect_four.datastore.PlayerID;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.Factory;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.FullBoard;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.FullGame;
+import edu.hm.scholz.enno.connect_four.datastore.mutable.FullPlayer;
+import edu.hm.scholz.enno.connect_four.logic.GameManager;
+import edu.hm.scholz.enno.connect_four.logic.LogicFactory;
+import edu.hm.scholz.enno.connect_four.view.ConnectFourPixelGridView;
+import edu.hm.scholz.enno.connect_four.view.ValidatingView;
+import edu.hm.scholz.enno.connect_four.view.View;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Theories.class)
+public class BotTheorieTest {
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(1_000);
+
+    private static int testRun;
+
+    @DataPoints("rep") public static List<Integer> repeat = IntStream.range(0, 1_000).boxed().collect(Collectors.toList());
+
+    @Theory
+    public void theorieTest(@FromDataPoints("rep")int repeats){
+        // Datastore
+        final FullPlayer player1 = Factory.makePlayer(PlayerID.PLAYER_1);
+        final FullPlayer player2 = Factory.makePlayer(PlayerID.PLAYER_2);
+        final FullBoard board = Factory.makeBoard();
+        final FullGame game = Factory.makeGame(PlayerID.PLAYER_1, board);
+
+        // Logik
+        final GameManager rules = LogicFactory.makeGameManager(board, game, player1, player2);
+
+        // Liste von Views
+        final ValidatingView valiView = new ValidatingView(game);
+        final List<View> views = List.of(valiView);
+
+        // Liste von Controls, eine fuer jeden Spieler
+        final List<Control> controls = List.of(
+                new Bot(game, rules, PlayerID.PLAYER_1),
+                new Bot(game, rules, PlayerID.PLAYER_2));
+
+        //Bot vs Bot setup required
+        game.setActivePlayer(PlayerID.PLAYER_1);
+        game.setPlayerCount(2);
+        game.setIsStarted(true);
+        board.setHighlight(List.of(Factory.makeField(0, 1, PlayerID.NONE)));
+
+        // Initiales update, Views zeigen Startzustand
+        views.forEach(view -> view.update(board, game, player1, player2));
+
+        // Fortfahren bis das Spiel beendet ist
+        while(game.getActivePlayer() != PlayerID.NONE)
+            controls.stream()
+                    .filter(Control::running) // noch beteiligte Spieler herausfiltern
+                    .forEach(Control::step); // jeder Spieler zieht
+
+        // Views und Controls abbauen
+        controls.forEach(Control::close);
+        views.forEach(View::shut);
+        System.out.println(testRun++);
+        assertTrue(valiView.isValidGame());
+    }
+}
