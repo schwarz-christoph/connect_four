acb56cf19599ed040f7f9a698f09dfabd8cd06c5
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2021-06-29 13:23:31 +0200

    Removed Board Full in ValidatingView

--- src/edu/hm/scholz/enno/connect_four/view/ValidatingView.java
+++ src/edu/hm/scholz/enno/connect_four/view/ValidatingView.java
@@ -51,7 +51,6 @@ public record ValidatingView(Game game) implements View {
         final boolean winnerPlayer2 = validateWinner(board, PlayerID.PLAYER_2);
         final List<Field> fields = board.getFields();
         final int fieldSize = 8;
-        final boolean boardFull = fields.size() == fieldSize * (fieldSize - 1);
 
         if (winnerPlayer1 ^ winnerPlayer2) // ^ = exclusive or
             wantPlayerID = winnerPlayer1 ? PlayerID.PLAYER_1 : PlayerID.PLAYER_2;
